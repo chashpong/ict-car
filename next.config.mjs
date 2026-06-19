072a0f5eb21b2c5a2ab991c840e706e3680bea3c
Chashpong <chashpong@example.com>
2026-06-19 14:19:15 +0700

    19/6

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',          // สั่งให้ Build ออกมาเป็นไฟล์ Static (HTML/CSS/JS)
-  basePath: '/ict-car',      // ตั้งค่าให้ตรงกับชื่อ Repository ของคุณบน GitHub
   typescript: {
     ignoreBuildErrors: true,
   },
--- out/_next/static/BXrHQW-qh3Np1s2JmL_cx/_buildManifest.js
+++ out/_next/static/BXrHQW-qh3Np1s2JmL_cx/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ict-car//_next/:path+",
+        "destination": "/ict-car/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
